e8138f942c66e78a5aa16529a85b6146da78f534
bridgetp24 <bridgetpeterkin@gmail.com>
2022-01-22 14:17:58 -0600

    Add popup window and logo

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
 // js file for project
+rebootButton.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: rebootPage,
+    });
+  });
+  
+  // The body of this function will be executed as a content script inside the
+  // current page
+  function rebootPage() {
+    // perform reboot page logic 
+  }
